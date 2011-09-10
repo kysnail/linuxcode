d4929e38b96ead4364325b5bc29e73b683480125
kysnail <LifeOfProgrammer@gmail.com>
2011-09-11 01:05:46 +0800

    add python source code.

---
+++ ZFPYTHON/Part 1/ch02/Chinese936.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# file: Chinese936.py
+#
+chinese = '''
+在 Python 中使用中文
+需要注意字符编码的问题
+可以使用的字符编码有如下几种：
+utf-8 cp036 gb2312 iso-8850-1
+'''
+print chinese.decode('utf-8').encode('cp936')
