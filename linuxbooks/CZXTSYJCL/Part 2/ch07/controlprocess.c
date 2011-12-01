3ef9e9b294adcd66605c8b4d48ad7351d52ac44b
kysnail <lifeofprogrammer@gmail.com>
2011-12-01 14:38:09 +0800

    demo 1 -> control process

---
+++ linuxbooks/CZXTSYJCL/Part 2/ch07/controlprocess.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+	int p1, p2, p3, p4, p5, p6, p7;
+	int end_p1 = 0, end_p2 = 0, end_p3 = 0, end_p4 = 0, end_p5 = 0, end_p6 = 0, end_p7 = 0; 	
+	int pid, status;
+
+	// Create Process 1
+	if ((p1 = fork()) == 0) 
+	{
+		execl("/bin/echo", "echo", "I am P1", 0);
+	}
+
+	wait(&status);
+	if ((p2 = fork()) == 0)
+	{
+		execl("/bin/echo", "echo", "I am P2", 0);
+	}
+}
