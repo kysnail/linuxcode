4c4dd4c120d32db009d82aeb87fe9c611ff680da
kysnail <lifeofprogrammer@gmail.com>
2011-12-03 20:55:37 +0800

    今后对于新创建的文件，哪怕是空的，也要先 add 操作一下，否则不好进行 diff 查看。

--- linuxbooks/CZXTSYJCL/Part 2/ch07/controlprocess05.c
+++ linuxbooks/CZXTSYJCL/Part 2/ch07/controlprocess05.c
@@ -34,10 +34,8 @@ int main()
 		execl("/bin/echo", "echo", "I am P3", 0);
 	}
 
-	// 这就是与 controlprocess03.c 的本质区别，这里使用了循环，等待 p2/p3 的完成，当然需要
-	// 注意的是，这里只要 p2/p3 中的任意一个返回即可，与下面的那个稍有不同。
-	// 也正是由于这个细微的差别，所以子进程执行顺序仍是混乱的，只能保证 p2/p3 中的一个领先
-	// 于 p4/p5 但另一个是否一定弄呢狗狗领先于 p4/p5 则不能确定。
+	// 这里连续使用了两个 wait 函数，只能保证 p2/p3 必定限于 p4/p5 完成，但并不能保证 p2/p3 之间的
+	// 先后顺序。
 	pid = wait(&status);
 	pid = wait(&status);
 	if (pid == p2) end_p2 = 1;
@@ -53,7 +51,7 @@ int main()
 		execl("/bin/echo", "echo", "I am P5", 0);
 	}
 
-	// 作用与上面的 do...while 类似，不同的地方在于，次循环可以保证 p4&p5 都完成后，再运行下一条指令。
+	// 这里连续使用了两个 wait 函数，保证 p4/p5 的完成，但并不能保证 p4/p5 之间的先后顺序。
 	pid = wait(&status);
 	pid = wait(&status);
 	if (pid == p4) end_p4 = 1;
