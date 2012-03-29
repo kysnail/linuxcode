f859c87d5c693c03c5bb1c55b78d3992104b9f1e
kysnail <lifeofprogrammer@gmail.com>
2012-03-29 10:14:17 +0800

    I try to do it by myself, for the first edition.

--- algorithmbooks/LJCDS/ch03/doublelinkedlist/InsertDList.c
+++ algorithmbooks/LJCDS/ch03/doublelinkedlist/InsertDList.c
@@ -1,4 +1,26 @@
-int InsertDList(DListLink head, int i, DataType e)
+/**
+ * Insert a node in double linked list. 
+ */
+int InsertDList(DLinkList head, int i, DataType e)
 {
-	
+        DLinkNode *p, *s;
+        int j;
+        p = head->next; // with head node
+
+        for (int j = 1; j != i; j++)
+        {
+                 p = p->next
+        }
+
+        if (j != (i - 1))
+        {
+                printf("wrong location\n");
+        }
+
+        *s = (DLinkNode)malloc(sizeof(struct DLinkNode));
+
+        p->prior->next = s;
+        s->prior = p->prior;
+        s->next = p;
+        p->prior = s;
 }
