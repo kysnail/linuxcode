57e10465f9aab35e3b8246109f704f21e65f1774
kysnail <TracingLife@gmail.com>
2012-05-30 00:19:57 +0800

    用 + 号连接字符串。

---
+++ rubybooks/ProgrammingRuby2nd/partI/chap02/src/0201.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby -w
+def say_goodnight(name)
+	result = "Good night, " + name
+	return result	
+end
+# time for bed...
+puts say_goodnight("John-Boy")
+puts say_goodnight("Mary-Ellen")
