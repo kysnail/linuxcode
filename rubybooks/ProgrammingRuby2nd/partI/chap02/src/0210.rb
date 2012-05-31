d51931002fa5d921652b19dd260348c0694873c7
kysnail <lifeofprogrammer@gmail.com>
2012-05-31 18:59:31 +0800

    演示 Ruby 的控制结构。

---
+++ rubybooks/ProgrammingRuby2nd/partI/chap02/src/0210.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby -w
+
+puts "演示 Ruby Control Structures"
+
+count = 80
+if count > 10
+    puts "Try again"
+elsif tries == 3
+    puts "You lose"
+else
+    puts "Enter a number"
+end
