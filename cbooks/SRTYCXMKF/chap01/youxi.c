fccecf1b7bf8898d932a67a7f7fcb513a950f250
kysnail <TracingLife@gmail.com>
2012-06-21 10:04:41 +0800

    完成如下工作： 1. 完成游戏方块结构体数组定义工作； 2. 完成函数的声明； 3. 引入相应头文件； 4. 定义所需按键码。

--- cbooks/SRTYCXMKF/chap01/youxi.c
+++ cbooks/SRTYCXMKF/chap01/youxi.c
@@ -1,3 +1,25 @@
+//----------------------------------------------------------------------------------------------------
+// Include Section
+//----------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <graphics.h>               // 图形函数库
+
+//----------------------------------------------------------------------------------------------------
+// Key Code Section
+//----------------------------------------------------------------------------------------------------
+// 定义按键码
+#define VK_LEFT     0x4b00
+#define VK_RIGHT    0x4d00
+#define VK_DOWN     0x5000
+#define VK_UP       0x4800
+#define VK_ESC      0x011b
+#define TIMER       0x1c            // 设置中断号
+
+//----------------------------------------------------------------------------------------------------
+// Data Structure Section
+//----------------------------------------------------------------------------------------------------
 struct BOARD        // 游戏底板结构，表示每个点所具有的属性。
 {
     int var;        // 当前状态只有 0 和 1，1 表示此点已被占用。
@@ -17,7 +39,7 @@ struct SHAPE
     char box[2];
     int color;      // 每个方块的颜色
     int next;       // 下个方块的编号
-}
+};
 
 // 初始化方块内容，即定义 MAX_BOX 个 SHAPE 类型的结构数组，并初始化。
 struct SHAPE shapes[MAX_BOX] =
@@ -30,6 +52,71 @@ struct SHAPE shapes[MAX_BOX] =
     { 0x88, 0xc0,   CYAN,   1},
     { 0xe8, 0x0,    CYAN,   2},
     { 0xc4, 0x40,   CYAN,   3},
-    { 0x2e, 0x0,    CYAN,   4},
+    { 0x2e, 0x0,    CYAN,   0},
 
-}
+    /*
+     *    □             □ □  □ □ □    
+     *    □    □        □        □ 
+     *  □ □    □ □ □ □  □           
+     */
+    { 0x44, 0xc0,   MAGENTA,   5},
+    { 0x8e, 0x0,    MAGENTA,   6},
+    { 0xc8, 0x80,   MAGENTA,   7},
+    { 0xe2, 0x0,    MAGENTA,   4},
+
+    /*
+     *    □          
+     *    □ □      □ □
+     *      □    □ □
+     */
+    { 0x8c, 0x40,   YELLOW,   9},
+    { 0x8e, 0x0,    YELLOW,   8},
+
+    /*
+     *      □          
+     *    □ □    □ □
+     *    □        □ □
+     */
+    { 0x4c, 0x80,   BROWN,   11},
+    { 0x6c, 0x0,    BROWN,   10},
+
+    /*
+     *         □    □ □ □     □    
+     *    □    □ □    □     □ □
+     *  □ □ □  □              □ 
+     */
+    { 0x4e, 0x0,     WHITE,   13},
+    { 0x8c, 0x80,    WHITE,   14},
+    { 0xe4, 0x0,     WHITE,   15},
+    { 0x4c, 0x40,    WHITE,   12},
+
+    /*     
+     *    □
+     *    □          
+     *    □    □ □ □ □
+     *    □             
+     */
+    { 0x88, 0x88,   RED,   17},
+    { 0xf0, 0x0,    RED,   16},
+
+    /*     
+     *    □ □
+     *    □ □            
+     */
+    { 0xcc, 0x0,    BLUE,   18}
+};
+
+//----------------------------------------------------------------------------------------------------
+// Function Declaration
+//----------------------------------------------------------------------------------------------------
+void interrupt newtimer(void);
+void SetTimer(void interrupt(*IntProc) (void));
+void KillTimer();
+void initialize(int x, int y, int m, int n);
+void DelFullRow(int y);
+void SetFullRow(int t_boardy);
+int MkNextBox(int box_numb);
+void EraseBox(int x, int y, int box_numb);
+void show_box(int x, int y, int box_numb, int color);
+int MoveAble(int x, int y, int box_numb, int direction);
+
