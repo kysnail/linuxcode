6c809d235127f043fe00ac9199774e2af14657ef
kysnail <LifeOfProgrammer@gmail.com>
2012-08-24 21:21:26 +0800

    add explain about umask function demo.

--- linuxbooks/Programming/W.RichardStevens/APUE2/ch04/src/fig4.9.c
+++ linuxbooks/Programming/W.RichardStevens/APUE2/ch04/src/fig4.9.c
@@ -23,8 +23,31 @@
 int
 main(void)
 {
-	//umask(0);
+	umask(0);
+	/* 
+ 	formula
+	-------
+	mode & ~cmask
+	
+	mode   = 110 110 110
+	cmask  = 000 000 000
+	~cmask = 111 111 111
+
+	mode & ~cmask = 110 110 110 => rw-rw-rw-
+	*/
 	if (creat("foo", RWRWRW) < 0)
 		err_sys("create error for foo");
+	umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+	/* 
+	mode   = 110 110 110
+	cmask  = 000 110 110
+	~cmask = 111 001 001
+
+	mode & ~cmask = 110 000 000 => rw-------
+	*/
+	if (creat("bar", RWRWRW) < 0)
+		err_sys("create error for bar");
+	
+	exit(0);
 }
 
